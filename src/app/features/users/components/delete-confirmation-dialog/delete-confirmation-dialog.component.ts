b02d08ce513d0afadbc25a7ed10faebe3e087c10
Supun Kavinda <naskavinda@gmail.com>
2025-02-06 05:01:49 +0530

    remove inline template

--- src/app/features/users/components/delete-confirmation-dialog/delete-confirmation-dialog.component.ts
+++ src/app/features/users/components/delete-confirmation-dialog/delete-confirmation-dialog.component.ts
@@ -6,24 +6,8 @@ import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/materia
   selector: 'app-delete-confirmation-dialog',
   standalone: true,
   imports: [MatDialogModule, MatButtonModule],
-  template: `
-    <h2 mat-dialog-title>Delete User</h2>
-    <mat-dialog-content>
-      Are you sure you want to delete user "{{data.username}}"?
-    </mat-dialog-content>
-    <mat-dialog-actions align="end">
-      <button mat-button mat-dialog-close>Cancel</button>
-      <button mat-button color="warn" [mat-dialog-close]="true">Delete</button>
-    </mat-dialog-actions>
-  `,
-  styles: [`
-    mat-dialog-content {
-      margin: 20px 0;
-    }
-    mat-dialog-actions {
-      padding: 16px 0;
-    }
-  `]
+  templateUrl: './delete-confirmation-dialog.component.html',
+  styleUrls: ['./delete-confirmation-dialog.component.scss']
 })
 export class DeleteConfirmationDialogComponent {
   constructor(
