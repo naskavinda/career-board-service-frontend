deec6d45e5f6ce9ca5a6f2b6c539362c5afd05b0
Supun Kavinda <naskavinda@gmail.com>
2025-02-09 21:04:49 +0530

    Add Loading message to the admin page

--- src/app/features/users/components/timeline/timeline.component.ts
+++ src/app/features/users/components/timeline/timeline.component.ts
@@ -3,16 +3,18 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../../../posts/models/user.model';
 import { UserPostsComponent } from '../../../posts/components/user-posts/user-posts.component';
 import { UserService } from '../../../posts/services/user.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-users-timeline',
   standalone: true,
-  imports: [CommonModule, UserPostsComponent],
+  imports: [CommonModule, UserPostsComponent, MatProgressSpinnerModule],
   templateUrl: './timeline.component.html',
   styleUrls: ['./timeline.component.scss'],
 })
 export class TimelineComponent implements OnInit {
   users: User[] = [];
+  loading = true;
 
   constructor(private userService: UserService) {}
 
@@ -21,12 +23,15 @@ export class TimelineComponent implements OnInit {
   }
 
   private fetchUsers() {
+    this.loading = true;
     this.userService.fetchUsers().subscribe({
       next: (data: User[]) => {
         this.users = data;
+        this.loading = false;
       },
       error: (error) => {
         console.error('Error fetching users:', error);
+        this.loading = false;
       },
     });
   }
